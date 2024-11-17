be6d3c15510870a6166c3a98ee9183d730432f16
Chris <chris.goringe@gmail.com>
2024-11-17 16:00:06 +1100

    347

--- js/groups.js
+++ js/groups.js
@@ -2,12 +2,25 @@ import { app } from "../../scripts/app.js"
 import { NodeInclusionManager } from "./node_inclusion.js"
 import { Colors, Texts } from "./constants.js"
 import { mode_change } from "./utilities.js"
+import { Debug } from "./debug.js"
+
+function selected_groups_and_childgroups() {
+    const sgac = Set()
+    function add_groups_recursively(g) {
+        sgac.add(g)
+        Array.from(g.children).filter((c)=>(c instanceof LGraphGroup)).forEach((c)=>{add_groups_recursively(c)})
+    }
+    app.graph._nodes.filter((g)=>(g.selected)).forEach((g)=>{add_groups_recursively(g)})
+    add_groups_recursively()
+    return sgac
+}
 
 export class GroupManager {
     static _instance = null
     constructor() {
         this.groups = {}  // maps group name to Set of node ids
         const ungrouped = new Set()
+        const selected = selected_groups_and_childgroups()
         app.graph._nodes.forEach((node)=>{
             if (NodeInclusionManager.node_includable(node)) ungrouped.add(node.id)
         })
@@ -16,7 +29,8 @@ export class GroupManager {
             if (!group.graph) {
                 group.graph = app.graph
             }
-            if (!app.canvas.selected_group_moving) group.recomputeInsideNodes()
+            if (!selected.has(group)) group.recomputeInsideNodes()
+            else Debug.trivia(`Not updating group ${group.title} because it is selected`)
             group._nodes.forEach((node) => {
                 if (NodeInclusionManager.node_includable(node)) {
                     if (!this.groups[group.title]) {
